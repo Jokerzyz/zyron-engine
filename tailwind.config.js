c13495eb2ee2969c6de672a2900f7421c260f560
zhenyuanzhang <zhenyuan.zhang@zhenyuanzhangdeMac-mini.local>
2026-08-13 10:09:34 +0800

    feat: improve auth feedback and search discovery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './guide.html', './src/**/*.{ts,css}'],
+  content: ['./*.html', './src/**/*.{ts,css}'],
   theme: {
     extend: {},
   },
